820557d2bd29c0cdf872326420f73e790c95418e
Drew Volz <volz.drew@gmail.com>
2018-08-26 13:29:27 -0600

    don't break semver with version checks

--- source/lib/constants.js
+++ source/lib/constants.js
@@ -6,10 +6,10 @@ export const GH_BASE_URL = 'https://github.com/StoDevX/AAO-React-Native'
 export const GH_NEW_ISSUE_URL = `${GH_BASE_URL}/issues/new`
 
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
-export const IS_ALPHA = version.includes('-alpha.')
-export const IS_BETA = version.includes('-beta.')
-export const IS_PRE = version.includes('-pre.')
-export const IS_RC = version.includes('-rc.')
+export const IS_ALPHA = version.includes('-alpha')
+export const IS_BETA = version.includes('-beta')
+export const IS_PRE = version.includes('-pre')
+export const IS_RC = version.includes('-rc')
 export const IS_DEBUG = IS_ALPHA || IS_BETA || IS_PRE || IS_RC
 
 const GOOGLE_ANALYTICS_PRODUCTION_ID = 'UA-90234209-2'
